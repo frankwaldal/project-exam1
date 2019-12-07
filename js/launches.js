d9176c57d910b8e5d8dff97d3dd483440efc4fb3
Frank Waldal <49303162+frankwaldal@users.noreply.github.com>
2019-12-07 01:19:26 +0100

    added interactive calendar with api integration for future launches

--- js/launches.js
+++ js/launches.js
@@ -1,72 +1,132 @@
-var today = new Date();
-
-var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-
-function calendar(month, year, date) {
-    if (date) {
-        console.log(`Today is ${date}. of ${months[month]}, ${year}.`);
-    } else {
-        console.log(`You want to see ${months[month]} in ${year}.`);
-    }
+function expandEvent() {
+    var url = `https://api.spacexdata.com/v3/launches/${this.className.substring(67,70)}`;
+    fetch(url)
+        .then(resolve => {
+            resolve.json().then(respond => {
+                var launchTime = '';
+                if (respond.last_ll_launch_date === null || respond.last_ll_launch_date === undefined) {
+                    var launch = new Date(respond.launch_date_utc);
+                    launchTime = `<p>Launchdate: ${launch.toString().substring(4,15)}</p>
+                        <p>Launchtime: ${launch.toLocaleTimeString()}</p>`;
+                } else {
+                    if (respond.launch_date_utc<respond.last_ll_launch_date) {
+                        var launch = new Date(respond.last_ll_launch_date);
+                        launchTime = `<p>Launchdate: ${launch.toString().substring(4,15)}</p>
+                            <p>Launchtime: ${launch.toLocaleTimeString()}</p>`;
+                    } else {
+                        var launch = new Date(respond.launch_date_utc);
+                        launchTime = `<p>Launchdate: ${launch.toString().substring(4,15)}</p>
+                            <p>Launchtime: ${launch.toLocaleTimeString()}</p>`;
+                    }
+                }
+                var cores = '';
+                for (i=0;i<respond.rocket.first_stage.cores.length;i++) {
+                    if (respond.rocket.first_stage.cores[i].core_serial === null || respond.rocket.first_stage.cores[i].core_serial === undefined) {
+                        cores += '   No information yet.';
+                    } else {
+                        cores += ` - ${respond.rocket.first_stage.cores[i].core_serial}`;
+                    }
+                }
+                var customers = '';
+                for (i=0;i<respond.rocket.second_stage.payloads[0].customers.length;i++) {
+                    customers += ` - ${respond.rocket.second_stage.payloads[0].customers[i]}`;
+                }
+                var details = '';
+                if (respond.details !== null) {
+                    details = `<p>${respond.details}</p><br><br>`;
+                }
+                var orbit = '';
+                if (respond.rocket.second_stage.payloads[0].orbit_params.regime === null || respond.rocket.second_stage.payloads[0].orbit_params.regime === undefined) {
+                    orbit = `${respond.rocket.second_stage.payloads[0].orbit}`
+                } else {
+                    orbit = `${respond.rocket.second_stage.payloads[0].orbit} - ${respond.rocket.second_stage.payloads[0].orbit_params.regime}`
+                }
+                var content = `<button id="close">X</button>
+                    <h3>${respond.mission_name}</h3>
+                    ${details}
+                    ${launchTime}
+                    <p>Launchsite: ${respond.launch_site.site_name_long}</p>
+                    <br>
+                    <br>
+                    <p>Rocket: ${respond.rocket.rocket_name}</p>
+                    <p>Core: ${cores.substring(3)}</p>
+                    <p>Payload: ${respond.rocket.second_stage.payloads[0].payload_type}</p>
+                    <p>Payload manufacturer: ${respond.rocket.second_stage.payloads[0].manufacturer}</p>
+                    <p>Payload customer: ${customers.substring(3)}</p>
+                    <p>Orbit: ${orbit}</p>`;
+                document.querySelector('#event').innerHTML = content;
+                document.querySelector('#event').className = 'open';
+                document.querySelector('#close').addEventListener('click', e => {
+                    e.preventDefault();
+                    document.querySelector('#event').className = 'closed';
+                });
+            })
+        })
+        .catch(err => {
+            document.querySelector('#event').innerHTML = `<button id="close">X</button>
+                <p>Couldn't retrieve information about launch.</p>
+                <p>Error: ${err}</p>`
+        })
 }
 
-calendar(today.getMonth(), today.getFullYear(), today.getDate());
+function calendarInit() {
+    var calendarContainer = document.querySelector('#calendar');
 
-function expandEvent(e) {
-    console.log(this.className.substring(67,70));
+    var eventArray = [];
+
+    fetch('https://api.spacexdata.com/v3/launches/upcoming')
+        .then(resolve => {
+            resolve.json().then(respond => {
+                for (i=0;i<respond.length;i++) {
+                    var start = '';
+                    if (respond[i].last_ll_launch_date === null || respond[i].last_ll_launch_date === undefined) {
+                        start = respond[i].launch_date_utc;
+                    } else {
+                        if (respond[i].launch_date_utc<respond[i].last_ll_launch_date) {
+                            start = respond[i].last_ll_launch_date;
+                        } else {
+                            start = respond[i].launch_date_utc;
+                        }
+                    }
+                    var flightNumber = respond[i].flight_number;
+                    var eventObject = {
+                        classNames: `flight_number${numeral(flightNumber).format('000')}`,
+                        title: `${respond[i].mission_name}`,
+                        start: `${start.substring(0,10)}`,
+                        backgroundColor: '#6897ba',
+                        borderColor: '#628eef',
+                        textColor: '#eee'
+                    }
+                    eventArray.push(eventObject);
+                }
+                var calendar = new FullCalendar.Calendar(calendarContainer, {
+                    plugins: ['dayGrid'],
+                    events: eventArray,
+                });
+
+                calendar.render();
+
+                document.querySelectorAll('.fc-event').forEach(item => {
+                    item.addEventListener('click', expandEvent);
+                });
+
+                document.querySelectorAll('.fc-button').forEach(item => {
+                    item.addEventListener('click', e => {
+                        setTimeout(function() {
+                            document.querySelectorAll('.fc-event').forEach(item => {
+                                item.addEventListener('click', expandEvent);
+                            })
+                        },200)
+                    })
+                })
+            })
+        })
 }
 
-function calendarTest() {
-    var futureLaunchCalendar = document.querySelector('#calendar');
+function pastLaunchPopulate() {
 
-    var arrayTest = [{
-        classNames: 'flight_number088',
-        title: 'Test one',
-        start: '2019-12-12'
-    },{
-        classNames: 'flight_number089',
-        title: 'Test two',
-        start: '2019-12-24'
-    }];
-
-    var calendar = new FullCalendar.Calendar(futureLaunchCalendar, {
-        plugins: ['dayGrid'],
-        events: arrayTest
-    });
-
-    calendar.render();
-
-    document.querySelectorAll('.fc-event').forEach(item => {
-        item.addEventListener('click', expandEvent);
-    });
 }
 
-calendarTest();
+calendarInit();
 
-
-
-
-
-
-/*
-var calendarEl = document.getElementById('calendar');
-
-var calendar = new FullCalendar.Calendar(calendarEl, {
-    plugins: [ 'dayGrid' ],
-    events: [{
-        title: 'Test Event',
-        start: '2019-12-12'
-    },{
-        title: 'Test Event 2',
-        start: '2019-12-13'
-    },{
-        title: 'Test Event 3',
-        start: '2020-01-10'
-    },{
-        title: 'Test Event 3',
-        start: '2020-02-10'
-    }]
-});
-
-calendar.render();
-*/
+pastLaunchPopulate();
